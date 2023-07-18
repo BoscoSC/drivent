2f01f0478bddc049d2fe5f8489cea921a2f4be0c
Joao Bosco <jboscos.carvalho@gmail.com>
2023-07-18 12:09:30 -0300

    bug fix: migrations fixed

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -19,6 +19,7 @@ export async function cleanDb() {
 }
 
 export async function generateValidToken(user?: User) {
+  console.log(process.env.JWT_SECRET);
   const incomingUser = user || (await createUser());
   const token = jwt.sign({ userId: incomingUser.id }, process.env.JWT_SECRET);
 
